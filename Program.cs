d0b43eb6feeb4c90a9b933f9d868e181bdd516ec
REGRAGUIahmed <ah.regragui@edu.umi.ac.ma>
2022-04-09 01:17:16 +0000

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			SR1.BanqueServiceClient stub = new SR1.BanqueServiceClient();
+			Console.WriteLine(stub.ConversionEuroToDH(80));
+			SR1.compte cp = stub.getCompte(5);
+			Console.WriteLine(cp.code);
+			Console.WriteLine(cp.solde);
+			SR1.compte[] cmptes = stub.listCompte();
+			foreach (var c in cmptes)
+			{
+				Console.WriteLine("-----------------------");
+				Console.WriteLine(c.code);
+				Console.WriteLine(c.solde);
+			}
+			Console.ReadLine();
+
+		}
+	}
+}
